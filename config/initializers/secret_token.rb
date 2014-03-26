228ebc756304a4e145c0a102cb132fcefa1f954d
rodmal52 <rodmal52@vm-0.rodmal52.koding.kd.io>
2014-03-26 02:10:28 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectCrowdmaps::Application.config.secret_key_base = 'a810cae33ab748373f89cdb2600ed985665d44f7bf6fcfbfc18df263b9a00ad53f55901a5fc4030c32a262f974fe2f635d414f663636befaa49c9db721c0dd01'
